a72117403a885661e6073f2fbda5fddaa4d00a31
unknown <alif_rayhan_27rpl@student.smktelkom-mlg.sch.id>
2020-12-07 21:55:02 +0700

    praktikum 5

---
+++ node_api.js
@@ -0,0 +1,120 @@
+const express = require("express")
+const app = express()
+const multer = require("multer") // untuk upload file
+const path = require("path") // untuk memanggil path direktori
+const fs = require("fs") // untuk manajemen file
+const mysql = require("mysql")
+const cors = require("cors")
+const md5 = require("md5")
+const Cryptr = require("cryptr")
+const crypt = new Cryptr("140533601726")
+
+app.use(express.static(__dirname));
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
+
+const dbs = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "pelanggaran_siswa"
+})
+
+// endpoint login user (authentication)
+app.post("/user/auth", (req, res) => {
+    // tampung username dan password
+    let param = [
+        req.body.username, //username
+        md5(req.body.password) // password
+    ]
+    
+
+    // create sql query
+    let sql = "select * from user where username = ? and password = ?"
+
+    // run query
+    dbs.query(sql, param, (error, result) => {
+        if (error) throw error
+
+        // cek jumlah data hasil query
+        if (result.length > 0) {
+            // user tersedia
+            res.json({
+                message: "Logged",
+                token: crypt.encrypt(result[0].id_user), // generate token
+                data: result
+            })
+        } else {
+            // user tidak tersedia
+            res.json({
+                message: "Invalid username/password"
+            })
+        }
+    })
+})
+
+validateToken = () => {
+    return (req, res, next) => {
+        // cek keberadaan "Token" pada request header
+        if (!req.get("Token")) {
+            // jika "Token" tidak ada
+            res.json({
+                message: "Access Forbidden"
+            })
+        } else {
+            // tampung nilai Token
+            let token  = req.get("Token")
+            
+            // decrypt token menjadi id_user
+            let decryptToken = crypt.decrypt(token)
+
+            // sql cek id_user
+            let sql = "select * from user where ?"
+
+            // set parameter
+            let param = { id_user: decryptToken}
+
+            // run query
+            dbs.query(sql, param, (error, result) => {
+                if (error) throw error
+                 // cek keberadaan id_user
+                if (result.length > 0) {
+                    // id_user tersedia
+                    next()
+                } else {
+                    // jika user tidak tersedia
+                    res.json({
+                        message: "Invalid Token"
+                    })
+                }
+            })
+        }
+
+    }
+}
+
+app.get("/siswa", validateToken(), (req,res) => {
+    let sql = "select * from siswa"
+    dbs.query(sql, (err, result) => {
+        if (err) {
+            throw err
+        }
+        else{
+            let response = {
+                count: result.length,
+                siswa   : result
+            }
+        
+            res.setHeader("Content-Type","application/json")
+            res.send(JSON.stringify(response))
+        }
+    })
+
+    
+})
+
+
+app.listen(8000, () =>{
+    console.log("Server run on port 8000");
+})
